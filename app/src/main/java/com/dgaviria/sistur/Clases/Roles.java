b3ffc2808e0b9dc93761e319b31251dc0b5b878f
felipedu1969 <jfduqueb@misena.edu.co>
2019-09-10 14:29:48 -0500

    Se mejora la estructura de almacenamiento de usuarios y roles en Firebase, ahora la estructura queda de la siguiente forma: usuarios > (nombre usuario) > (datos del usuario): usuario (nombre del usuario), nombre (nombre de pila del usuario), contraseña (por ahora no está encriptada), correo (validado para que tenga un correo bien escrito), rolsuper (superusuario), roladmin (administrador app), rolgestor (gestor del CDI), rolcompras (usuario que compra), rolbasico (usuario básico) rol > (nombre del rol) > miembros > (nombre del usuario) > activo=(true,false) Se crean las clases para el usuario y el rol, siendo que a traves de estas se van a construir los datos a enviar a la BD. Solo el usuario con rol Superusuario debe crearse de forma manual respetando la estructura. Por el momento, los usuarios con rol Superusuario y Administrador tienen acceso directo a la creación de usuarios. Ellos al ingresar, llegan a la pantalla de creación de usuarios provisionalmente.

---
+++ app/src/main/java/com/dgaviria/sistur/Clases/Roles.java
@@ -0,0 +1,21 @@
+package com.dgaviria.sistur.Clases;
+
+public class Roles {
+
+    public Boolean activo;
+
+    public Roles() {
+    }
+
+    public Roles(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}
