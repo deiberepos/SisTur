5a9175c9c848a10adb46816051705ef5eb58c51f
felipedu1969 <jfduqueb@misena.edu.co>
2019-09-16 17:37:45 -0500

    Se modifican los colores base, se ajustan colores y letras del layout de Censo poblacional (pendiente el de CDI). Se crea en la base de datos de firebase la estructura de los menús dinámicos, se crea el POJO y el recycler del menú principal. Para la creación de usuarios se agraga un layout para seleccionar el tipo de acción a realizar: Crear nuevo o modificar existentes. En la de modificar existentes, se despliega el recycler de usuarios donde se adicionaron dos iconos que sirven para actualizar el usuario y borrar el usuario (lo modifiqué agregando un alertdialog). Se crea una nueva carpeta para organizar menus. Se elimina el menú que creó Deiber ya que se reemplazó por el recycler nuevo. Se adicionan los iconos de los menús y se referencian en la base de datos. Los recycler a veces no funcionan de una, requieren dos acciones para funcionar.

--- app/src/main/java/com/dgaviria/sistur/Adaptadores/AdaptadorMenuPpal.java
+++ app/src/main/java/com/dgaviria/sistur/Adaptadores/AdaptadorMenuPpal.java
@@ -2,7 +2,6 @@ package com.dgaviria.sistur.Adaptadores;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
