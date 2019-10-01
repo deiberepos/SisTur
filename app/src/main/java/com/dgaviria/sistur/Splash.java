e9d4db471eaa7888440ab6bf63b17ca175ba61c8
LTriana2234 <lgtriana8@misena.edu.co>
2019-09-30 21:27:37 -0500

    se modifica el splash

--- app/src/main/java/com/dgaviria/sistur/Splash.java
+++ app/src/main/java/com/dgaviria/sistur/Splash.java
@@ -1,16 +1,22 @@
 package com.dgaviria.sistur;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
+import com.daimajia.androidanimations.library.Techniques;
 import com.dgaviria.sistur.usuarios.LogueoUsuarios;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-public class Splash extends AppCompatActivity {
-    ImageView imagen;
+public class Splash extends AwesomeSplash {
+    /*ImageView imagen;
     private final int DURACION_SPLASH = 2000;
     
     
@@ -27,5 +33,39 @@ public class Splash extends AppCompatActivity {
                 finish();
             }
         }, DURACION_SPLASH);
+    }*/
+
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        configSplash.setBackgroundColor(R.color.colorAzulpastel);
+        configSplash.setAnimCircularRevealDuration(900);
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM);
+
+
+        configSplash.setLogoSplash(R.drawable.nuevo__logo3);
+        configSplash.setAnimLogoSplashDuration(5000);
+        configSplash.setAnimLogoSplashTechnique(Techniques.BounceInLeft);
+
+        configSplash.setTitleSplash("Sistur Turmina");
+        configSplash.setTitleTextColor(R.color.colorNegro);
+        configSplash.setTitleTextSize(30f);
+        configSplash.setAnimTitleDuration(3500);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(Splash.this , LogueoUsuarios.class));
     }
 }
